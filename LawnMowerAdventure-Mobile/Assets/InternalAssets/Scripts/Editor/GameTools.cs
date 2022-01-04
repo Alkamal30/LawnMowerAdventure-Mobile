613574c784d103c4f8b812ba384c1b07fde8f7c1
Rimuru <Rimuru.Dev@gmail.com>
2022-01-04 18:18:25 +0400

    Added the function of creating a screenshot of the game screen.

--- LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/Editor/GameTools.cs
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/Editor/GameTools.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,4 +7,17 @@ public sealed class GameTools : Editor
 {
     [MenuItem("GameTools/Clear all PlayerPrefs database")]
     public static void ClearAllPlayerPrefs() => PlayerPrefs.DeleteAll();
+
+    [MenuItem("GameTools/Take Screenshot Game Screen")]
+    public static void TakeScreenshotGameScreen()
+    {
+        string path = "GameTools/Screenshot";
+        string fileName = $"{DateTime.Now.Year}.{DateTime.Now.Month}.{DateTime.Now.Day} [{DateTime.Now.Hour}.{DateTime.Now.Minute}.{DateTime.Now.Second}-{DateTime.Now.Millisecond}].png";
+
+        Directory.CreateDirectory(path);
+
+        while (File.Exists($"{path}/{fileName}")) { }
+
+        ScreenCapture.CaptureScreenshot($"{path}/{fileName}");
+    }
 }
