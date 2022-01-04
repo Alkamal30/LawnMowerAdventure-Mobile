75fdb121118afe4ac6340a1002a324c71a3f821c
Rimuru <Rimuru.Dev@gmail.com>
2022-01-04 17:11:03 +0400

    The function of complete cleaning of the database PlayerPrefs has been added to the Unity editor.

--- LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/Editor/GameTools.cs
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/Editor/GameTools.cs
@@ -1,6 +1,8 @@
 using UnityEditor;
+using UnityEngine;
 
 public sealed class GameTools : Editor
 {
-
+    [MenuItem("GameTools/Clear all PlayerPrefs database")]
+    public static void ClearAllPlayerPrefs() => PlayerPrefs.DeleteAll();
 }
