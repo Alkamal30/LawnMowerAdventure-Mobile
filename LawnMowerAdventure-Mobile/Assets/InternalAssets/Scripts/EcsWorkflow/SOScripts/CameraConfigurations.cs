78d62a5525023242ed7b145b0c19e7e721905463
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 19:58:34 +0400

    Added CameraConfigurations.cs
    Stores parameters for setting up the main camera on a level.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/CameraConfigurations.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Camera Configurations", menuName = "Create New Camera Configurations", order = 0)]
+    public sealed class CameraConfigurations : ScriptableObject
+    {
+        public Vector3 currentVelocity = default;   // Default setting (0, 0, 0)
+        public Vector3 offset = default;            // Default setting (2.5, 4.5, -3)
+        public float cameraSmoothness = default;    // Default setting (0.05)
+    }
+}
