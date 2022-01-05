a36e614977e2f94745f4e7e970849156f192309d
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 20:01:17 +0400

    Added CoinSpawnSettings.cs
    Settings for randomly generating coins at levels.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/CoinSpawnSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Coin Spawn Settings", menuName = "Create New Coin Spawn Settings", order = 0)]
+    public sealed class CoinSpawnSettings : ScriptableObject
+    {
+        [Range(0f, 100f)]
+        public float lowerBoundOfCoinSpawn;
+        [Range(0f, 20f)]
+        public float coinSpawnRate;
+    }
+}
