ecd4f63c4efeeeee8f752323d94288891277596e
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 20:05:12 +0400

    Added WoodLogTrapSettings.cs
    Settings for falling tree mechanics.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/WoodLogTrapSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Wood Log Trap Settings", menuName = "Create Wood Log Trap Settings", order = 0)]
+    public sealed class WoodLogTrapSettings : ScriptableObject
+    {
+        public GameObject fallingLogPrefab = default;
+        public float startPositionY = 2.12f;
+        public float liftingIndent = 1f;
+        public bool isLiftingIndent = true;
+    }
+}
