9d64ca5549590a976da0d9bb49c26ac9003852e2
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 19:59:28 +0400

    Added CharacterInputSettings.cs
    Stores the settings required for turning the mower. Temporary solution.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/CharacterInputSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Character Input Settings", menuName = "Create New Character Input Settings", order = 0)]
+    public sealed class CharacterInputSettings : ScriptableObject
+    {
+        public float rotateTo = 0f;
+    }
+}
