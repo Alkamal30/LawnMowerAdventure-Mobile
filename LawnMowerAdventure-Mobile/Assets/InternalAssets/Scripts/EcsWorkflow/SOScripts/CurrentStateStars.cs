72e31a2419fc182a98f651cc48ed27f1d8b13388
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 20:02:35 +0400

    Added CurrentStateStars.cs
    Stores the settings for the state of the passage of the level.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/CurrentStateStars.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Current State Stars", menuName = "Create New Current State Stars", order = 0)]
+    public sealed class CurrentStateStars : ScriptableObject
+    {
+        public bool isFirstStar = false;
+        [FormerlySerializedAs("isSecondtStar")] public bool isSecondStar = false;
+        public bool isThreeStar = false;
+    }
+}
