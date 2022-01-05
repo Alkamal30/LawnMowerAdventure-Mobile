6d969da630f00dedde6e35674072296f7a837a56
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 20:07:01 +0400

    Added WinSettings.cs
    Settings for the level passing collider.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/WinSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Win Settings", menuName = "Create New Win Settings", order = 0)]
+    public sealed class WinSettings : ScriptableObject
+    {
+        public Collider winCollider;
+        public bool isCompletionOfTheLevel = false;
+    }
+}
