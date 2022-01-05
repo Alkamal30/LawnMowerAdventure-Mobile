1515cf99896ad698647e5ef119f1e81b5397f8cb
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 20:03:07 +0400

    Added LawnMower.cs
    Lawnmower settings.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/LawnMower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Lawn Mower", menuName = "Create Lawn Mower", order = 0)]
+    public sealed class LawnMower : ScriptableObject
+    {
+        public new string name;
+        public GameObject prefab;
+        [Space]
+        public float speed;
+        public int price;
+
+    }
+}
