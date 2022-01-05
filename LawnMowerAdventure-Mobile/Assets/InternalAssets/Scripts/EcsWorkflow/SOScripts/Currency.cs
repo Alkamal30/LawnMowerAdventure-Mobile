c48fe80801238d2809f78bf7979bcc7ce97e3c1c
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 20:01:48 +0400

    Added Currency.cs
    Game currency settings.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/Currency.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Currency ", menuName = "Create New Currency ", order = 0)]
+    public sealed class Currency : ScriptableObject
+    {
+        public GameObject Coin;
+        public float coinAddForce = 15f;
+    }
+}
