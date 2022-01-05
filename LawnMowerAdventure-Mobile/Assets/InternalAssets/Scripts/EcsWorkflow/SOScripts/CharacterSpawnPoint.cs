070ab6a7304d5d3757258336187ed292c9d78522
Rimuru <Rimuru.Dev@gmail.com>
2022-01-05 20:00:20 +0400

    Added CharacterSpawnPoint.cs
    Stores the settings for the spawn point of the lawnmower when starting the game.

---
+++ LawnMowerAdventure-Mobile/Assets/InternalAssets/Scripts/EcsWorkflow/SOScripts/CharacterSpawnPoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace AbyssMothGames.LawnMowerWorld
+{
+    [CreateAssetMenu(fileName = "New Character Spawn Point", menuName = "Create New Character Spawn Point", order = 0)]
+    public sealed class CharacterSpawnPoint : ScriptableObject
+    {
+        public Transform spawnPoint = default;
+    }
+}
